0c616a766389ed9fdf109fbff9359f4cef030676
khalidmansoor0310 <khalidmansoor725@gmail.com>
2022-03-04 03:54:16 -0800

    changes

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                         <div class="card mb-3">
                             <div class="row g-0">
                                 <div class="col-md-4">
-                                    <img src="images/events/event-3.jpg" class="img-fluid rounded-start" alt="...">
+                                    <img src="images/event-3.jpg" class="img-fluid rounded-start" alt="...">
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body">
